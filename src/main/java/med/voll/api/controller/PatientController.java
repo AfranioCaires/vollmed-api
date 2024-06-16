97a86a9f859316ad509f6d4269eada0bc386cf24
Afranio Caires <137647669+AfranioCaires@users.noreply.github.com>
2024-06-16 10:52:44 -0300

    feat: add patients GET method
    - Implemented a new GET endpoint for retrieving patients
    - Added support for filtering and sorting patient data
    - Included pagination parameters (size, page, sort) for the GET endpoint

--- src/main/java/med/voll/api/controller/PatientController.java
+++ src/main/java/med/voll/api/controller/PatientController.java
@@ -3,13 +3,14 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.patient.Patient;
+import med.voll.api.patient.PatientListData;
 import med.voll.api.patient.PatientRegisterData;
 import med.voll.api.patient.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("pacientes")
@@ -23,4 +24,12 @@ public class PatientController {
     public void registerPatient (@RequestBody @Valid PatientRegisterData data){
         patientRepository.save(new Patient(data));
     }
+
+    @GetMapping
+    public Page<PatientListData> patientListData(@PageableDefault(size = 10, sort = {"name"})Pageable pageable){
+        return patientRepository
+                .findAllByActiveTrue(pageable)
+                .map(PatientListData::new);
+    }
+
 }
