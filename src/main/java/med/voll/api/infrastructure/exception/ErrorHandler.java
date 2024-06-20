9aa4b43cf1b15f3cc3e4360728cb571fc74b82dd
Afranio Caires <137647669+AfranioCaires@users.noreply.github.com>
2024-06-19 23:22:21 -0300

    improvement: add detailed error handling for various exceptions
    - Added @ExceptionHandler for handling specific exceptions:
      - HttpMessageNotReadableException: returns 400 status with a descriptive message
      - BadCredentialsException: returns 401 status with 'Invalid credentials' message
      - AuthenticationException: returns 401 status with 'Authentication failed' message
      - AccessDeniedException: returns 403 status with 'Access denied' message
      - General Exception: returns 500 status with detailed error message
    - Improved overall error handling to provide more meaningful responses and status codes

--- src/main/java/med/voll/api/infrastructure/exception/ErrorHandler.java
+++ src/main/java/med/voll/api/infrastructure/exception/ErrorHandler.java
@@ -3,6 +3,10 @@ package med.voll.api.infrastructure.exception;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -32,6 +36,31 @@ public class ErrorHandler {
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
                 .build();
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity handleBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity handleAuthenticationException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Erro ao logar");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity handleAccessDenied() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity handle500(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " + ex.getLocalizedMessage());
     }
 
     private record ValidationErrorData (String filed, String message){
